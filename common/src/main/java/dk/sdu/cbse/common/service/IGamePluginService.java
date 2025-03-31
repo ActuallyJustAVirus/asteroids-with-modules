3204cf62fdd662201ad12d9758419e425db8a606
ActuallyJustAVirus <bern64390@gmail.com>
2025-03-31 21:12:37 +0200

    Add javadoc to services

--- common/src/main/java/dk/sdu/cbse/common/service/IGamePluginService.java
+++ common/src/main/java/dk/sdu/cbse/common/service/IGamePluginService.java
@@ -4,7 +4,19 @@ import dk.sdu.cbse.common.data.GameData;
 import dk.sdu.cbse.common.data.World;
 
 public interface IGamePluginService {
+    /**
+     * This method is called when the game starts. It is used to initialize the plugin and add entities to the world.
+     *
+     * @param gameData The game data object containing information about the game state.
+     * @param world    The world object representing the game world.
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * This method is called when the game stops. It is used to clean up the plugin and remove entities from the world.
+     *
+     * @param gameData The game data object containing information about the game state.
+     * @param world    The world object representing the game world.
+     */
     void stop(GameData gameData, World world);
 }
--- common/src/main/java/dk/sdu/cbse/common/service/IProcessService.java
+++ common/src/main/java/dk/sdu/cbse/common/service/IProcessService.java
@@ -4,5 +4,12 @@ import dk.sdu.cbse.common.data.GameData;
 import dk.sdu.cbse.common.data.World;
 
 public interface IProcessService {
+    /**
+     * Process the game data and world state.
+     * This method is called every frame to update the game state.
+     *
+     * @param gameData The current game data.
+     * @param world    The current world state.
+     */
     void process(GameData gameData, World world);
 }
